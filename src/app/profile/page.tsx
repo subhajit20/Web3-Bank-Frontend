637ac4d33a6073132ca144d91de88a1e38d8baad
Subhajit Ghosh <99127578+subhajit20@users.noreply.github.com>
2023-09-04 17:14:34 +0000

    Added profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Profile from "@/components/profile/Profile";
+import React from "react";
+
+const page = () => {
+  return (
+    <React.Fragment>
+      <Profile />
+    </React.Fragment>
+  );
+};
+
+export default page;
