196ab3bf8dcb6c52476a1ea793133a810806b030
Subhajit Ghosh <99127578+subhajit20@users.noreply.github.com>
2023-09-05 17:32:10 +0000

    Refactored profile page

--- src/components/ui/inputs/InputWithIcon.tsx
+++ src/components/ui/inputs/InputWithIcon.tsx
@@ -2,14 +2,19 @@ import React from "react";
 
 interface InputProps {
   icon?: any | string[];
-  _className?:string
+  _className?: string;
 }
 
 function InputWithIcon(props: InputProps) {
   return (
     <div className={props._className}>
-      <span className="justify-self-center place-self-center">{props.icon}</span>
-      <input className="input input-primary justify-self-start" placeholder="Primary" />
+      <span className="justify-self-center place-self-center">
+        {props.icon}
+      </span>
+      <input
+        className="input input-primary justify-self-start"
+        placeholder="Primary"
+      />
     </div>
   );
 }
